8b93f5035ad32941043942aaed0d6c25fa7878a8
Kong-16 <63219508+Kong-16@users.noreply.github.com>
2022-10-24 13:31:56 +0900

    Add files via upload

--- Algorithm/Sort/15975.py
+++ Algorithm/Sort/15975.py
@@ -1,19 +1,28 @@
+# Sort
 import sys
 
 input = sys.stdin.readline
 
 N = int(input())
 
-arr = [[] for _ in range(N)]
+arr = [[] for _ in range(N + 1)]
 
 for _ in range(N):
     p, c = map(int, input().split())
     arr[c].append(p)
 
-for i in arr:
+sorted_arr = [i for i in arr if len(i) > 1]
+for i in sorted_arr:
     i.sort()
 
-new_arr = []
-rem_arr = []
+dis_sum = 0
+for i in sorted_arr:
+    for j in range(len(i)):
+        if j == 0:
+            dis_sum += i[j + 1] - i[j]
+        elif j == len(i) - 1:
+            dis_sum += i[j] - i[j - 1]
+        else:
+            dis_sum += min(i[j] - i[j - 1], i[j + 1] - i[j])
 
-print(arr)
+print(dis_sum)
